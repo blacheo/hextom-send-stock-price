725e5cc1cf7ee7fd0393278beccb1cb0f1ddcd72
blaicheo <78671393+blacheo@users.noreply.github.com>
2025-09-04 15:33:28 -0400

    added openapi for email text generation

---
+++ backend/subscriptions/emails/chatgpt.py
@@ -0,0 +1,17 @@
+from decouple import config
+from openai import OpenAI
+
+CHATGPT_KEY = config("SECRET_KEY")
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=CHATGPT_KEY
+)
+
+def stock_advice(stock_sticker: str):
+    response = client.responses.create(
+        model="gpt-5-mini",
+        tools= [{ "type" : "web_search "}],
+        input=f"Should I buy, sell or hold {stock_sticker}. Only include a very short and concise reason for the decision. Do not include anything else"
+    )
+    return response.output_text
