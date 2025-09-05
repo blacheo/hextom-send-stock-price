bbc6fd481973374d4ec627b2fb617bc4651ead37
blaicheo <78671393+blacheo@users.noreply.github.com>
2025-09-04 21:42:37 -0400

    updated vite so that it builds to a single index file for github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import flowbiteReact from "flowbite-react/plugin/vite";
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
+  plugins: [react(), tailwindcss(), flowbiteReact(), viteSingleFile()],
 })
