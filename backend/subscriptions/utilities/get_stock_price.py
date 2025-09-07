02b4c33beaff78980b76b0cb0ad1add8033e2276
blaicheo <78671393+blacheo@users.noreply.github.com>
2025-09-06 22:34:47 -0400

    fixed formatting of prices

--- backend/subscriptions/utilities/get_stock_price.py
+++ backend/subscriptions/utilities/get_stock_price.py
@@ -4,4 +4,4 @@ from subscriptions.models import Subscription
 
 def get_stock_price(stock_sticker: str):
     ticker = Ticker(stock_sticker)
-    return ticker.info['regularMarketPrice']
+    return '{:,.2f}'.format(ticker.info['regularMarketPrice'])
